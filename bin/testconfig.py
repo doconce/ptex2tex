08c35dcc5b1e36f13412cb34a6ac059ab109b422
langtangen <langtangen@fef8ef5b-6c49-0410-a1a1-3917990f4dc1>
2009-03-29 08:25:50 +0000

    Updated documentation. Made testconfig.py for creating demos of environments in a config file. Use it in doc.p.tex.

--- bin/testconfig.py
+++ bin/testconfig.py
@@ -17,21 +17,46 @@ for line in f:
         if envir_type not in ('preprocess', 'inline_code', 'names'):
             envir_types.append(envir_type)
 f.close()
+names = open('tmp_names', 'w')
 index = 1
 for e in envir_types:
-    print 'n%d = %s' % (index, e)
+    names.write('n%d = %s\n' % (index, e))
     index += 1
-print '\n\n\n'
+names.close()
+print """
+A [names] section is written to the file tmp_names and should
+be appended to the .ptex2tex.cfg file in the current directory.
+"""
 
+latex = open('tmp_latex', 'w')
 for i in range(1, index):
-    print r"""
+    if envir_types[i-1] in ('Warnings', 'Tip', 'Note'):
+        
+        latex.write(r"""
 \noindent
-Here is environment %s:
+Here is a demo of the environment '%s':
 \bn%d
-v0 = 5
-g = 9.81
-t = 0.6
-y = v0*t - 0.5*g*t**2
+Some message can be written here as ordinary
+text.
+\en%d
+
+""" % (envir_types[i-1].replace('_', '\\_'), i, i))
+    else:
+        latex.write(r"""
+\noindent
+Here is a demo of the environment '%s':
+\bn%d
+v0 = 5                    # velocity
+g = 9.81                  # acceleration of gravity
+t = 0.6                   # time
+y = v0*t - 0.5*g*t**2     # vertical position
 print y
 \en%d
-""" % (envir_types[i-1].replace('_', '\\_'), i, i)
+
+""" % (envir_types[i-1].replace('_', '\\_'), i, i))
+latex.close()
+print """\
+A LaTeX demo code of all environments defined in .ptex2tex.cfg
+is written to the file tmp_latex and should be included
+in some LaTeX document.
+"""
